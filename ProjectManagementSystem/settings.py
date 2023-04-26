350797207bbd492a8a7abded953e447db2dc529d
Vishesh Patel <76718100+vishesh147@users.noreply.github.com>
2023-04-26 17:09:36 +0530

    Update settings.py

--- ProjectManagementSystem/settings.py
+++ ProjectManagementSystem/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'ProjectManagementSystemApp',
     'tinymce',
+    'whitenoise.runserver_nostatic',
 ]
 
 MIDDLEWARE = [
@@ -54,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'ProjectManagementSystem.urls'
